bb77d0cf123a7a12e4560d2a41518a398e16fa50
jose.pereda <jose.pereda@gluonhq.com>
2022-08-09 19:45:34 +0200

    Add user Sign up, and post badges via RF

---
+++ src/main/java/com/devoxx/badges/views/ActivatePresenter.java
@@ -0,0 +1,55 @@
+package com.devoxx.badges.views;
+
+import com.gluonhq.attach.settings.SettingsService;
+import com.gluonhq.charm.glisten.application.AppManager;
+import com.gluonhq.charm.glisten.control.AppBar;
+import com.gluonhq.charm.glisten.control.TextField;
+import com.gluonhq.charm.glisten.mvc.View;
+import com.gluonhq.charm.glisten.visual.MaterialDesignIcon;
+import javafx.beans.binding.Bindings;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.util.ResourceBundle;
+import java.util.regex.Pattern;
+
+public class ActivatePresenter {
+
+    private static final Pattern emailPattern = Pattern
+            .compile("[a-zA-Z0-9[!#$%&'()*+,/\\-_\\.\"]]+@[a-zA-Z0-9[!#$%&'()*+,/\\-_\"]]+\\.[a-zA-Z0-9[!#$%&'()*+,/\\-_\"\\.]]+");
+
+    @FXML
+    private View activateView;
+    @FXML
+    private TextField username;
+    @FXML
+    private Button submit;
+    @FXML
+    private ResourceBundle resources;
+
+    public void initialize() {
+        activateView.setOnShowing(event -> {
+            AppBar appBar = AppManager.getInstance().getAppBar();
+            appBar.setNavIcon(MaterialDesignIcon.ARROW_BACK.button(e ->
+                    AppManager.getInstance().switchToPreviousView()));
+            appBar.setTitleText(resources.getString("ACTIVATION.VIEW"));
+            username.setText(null);
+        });
+
+        submit.disableProperty().bind(Bindings.createBooleanBinding(() -> !isValidEmail(username.getText()), username.textProperty()));
+    }
+
+    @FXML
+    private void submit() {
+        SettingsService.create().ifPresent(settingsService ->
+                settingsService.store(AppViewManager.SAVED_ACCOUNT_EMAIL, username.getText()));
+        AppManager.getInstance().switchToPreviousView();
+    }
+
+    public static boolean isValidEmail(String email) {
+        if (email == null || email.isEmpty()) {
+            return false;
+        }
+        return emailPattern.matcher(email).matches();
+    }
+}
--- src/main/java/com/devoxx/badges/views/AppViewManager.java
+++ src/main/java/com/devoxx/badges/views/AppViewManager.java
@@ -19,13 +19,15 @@ import static com.gluonhq.charm.glisten.afterburner.AppView.Flag.SKIP_VIEW_STACK
 public class AppViewManager {
 
     public static final AppViewRegistry REGISTRY = new AppViewRegistry();
+    public static final String SAVED_ACCOUNT_EMAIL = "devoxx_badges_account_email";
 
     private static final ResourceBundle bundle = ResourceBundle.getBundle("com.devoxx.badges.views.drawer");
 
-    public static final AppView MAIN_VIEW = view("Main", MainPresenter.class, MaterialDesignIcon.HOME, SHOW_IN_DRAWER, HOME_VIEW, SKIP_VIEW_STACK);
+    public static final AppView MAIN_VIEW = view("Intro", MainPresenter.class, MaterialDesignIcon.HOME, SHOW_IN_DRAWER, HOME_VIEW, SKIP_VIEW_STACK);
     public static final AppView BADGES_VIEW = view("Badges", BadgesPresenter.class, MaterialDesignIcon.SCANNER, SHOW_IN_DRAWER);
     public static final AppView EDITION_VIEW = view("Badge Edition", EditionPresenter.class, MaterialDesignIcon.CONTACTS);
-    
+    public static final AppView SIGN_UP_VIEW = view("Sign up", ActivatePresenter.class, MaterialDesignIcon.LOCK);
+
     private static AppView view(String title, Class<?> presenterClass, MaterialDesignIcon menuIcon, AppView.Flag... flags ) {
         return REGISTRY.createView(name(presenterClass), title, presenterClass, menuIcon, flags);
     }
